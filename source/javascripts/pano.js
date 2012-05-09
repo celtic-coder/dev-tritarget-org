d3d2684ed95ea6d3e5dbff069aae2925eea6a307
Devin Weaver <suki@tritarget.org>
2012-05-09 18:40:35 -0400

    Fixes bad fancybox iphone interaction

--- source/javascripts/pano.js
+++ source/javascripts/pano.js
@@ -1,11 +1,16 @@
 var pano = {};
 
-pano.open = function (e) {
-    e.preventDefault();
+pano.isIOS = function () {
     // detect browser
-    var deviceAgent = navigator.userAgent.toLowerCase();
+    return navigator.userAgent.
+        toLowerCase().
+        match(/(iphone|ipod|ipad)/);
+};
+
+pano.openPano = function (e) {
+    e.preventDefault();
     var el = jQuery(this);
-    if (deviceAgent.match(/(iphone|ipod|ipad)/)) {
+    if (pano.isIOS()) {
         window.location.href = el.data('vr5-path');
     } else {
         jQuery.fancybox.open({
