0e611a0e396ceb89a4ef9e26f5f569be2e22fdcf
Devin Weaver <suki@tritarget.org>
2013-01-09 21:45:14 -0500

    Fixes short urls order

--- source/s/urls.js
+++ source/s/urls.js
@@ -1,6 +1,6 @@
 var __site_short_urls = {
   "p1": "http://github.com/sukima/NotesApp-Sample/",
   "p2": "http://sukima.github.com/redmine_equipment_status_viewer/",
-  "p3": "http://sukima.github.com/skiQuery/",
-  "p4": "http://github.com/sukima/SimNotify/"
+  "p3": "http://github.com/sukima/SimNotify/",
+  "p4": "http://sukima.github.com/skiQuery/"
 };
