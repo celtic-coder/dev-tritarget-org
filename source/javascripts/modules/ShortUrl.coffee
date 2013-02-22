28bc368dce2b08933f5ecbb3125ee670ea97cf32
Devin Weaver <suki@tritarget.org>
2013-02-22 15:05:49 -0500

    Refactors to use jQuery promises

--- source/javascripts/modules/ShortUrl.coffee
+++ source/javascripts/modules/ShortUrl.coffee
@@ -9,22 +9,64 @@
 # `http://mysite.com/s/#1` will redirect to the associated url for the id 1.
 #
 # This assumes that you have an index.html in the /s directory of your website.
-# It also has HTML elements it interacts with and is dependent on jQuery.
+# It is dependent on jQuery.
 #
 # ## Usage ##
 #     var s = new ShortUrl("path/to/json");
 #     s.setPath("http://mysite.com/s/");
+#     s.setOutputWith(function(data) {
+#       console.log(data);
+#     });
 #     s.ready();
+#
+# #### Date Object ####
+# The callback for setOutputWith will send a data object of the form:
+#     {
+#       id: 1,                      // The short id
+#       url: "http://example.com/", // The URL the short id points to
+#       short_path: "/s/#1"         // The path used for the sortened URL
+#     }
+#
+# #### Events ####
+# The following events are triggered:
+# * `redirect` - Called when the browser is about to redirect. Used to offer
+#   feed back to the user just before the redirect.
+# * `error` - Used to display any errors encountered. Passes a jQuery event
+#   object.
+#
+# Example:
+#     var s = new ShortUrl("path/to/json");
+#     $(s).on("redirect", function(data) {
+#       console.log(data);
+#     });
+#     $(s).on("error", function(event) {
+#       console.log(event);
+#     });
 $ = jQuery
 
 class ShortUrl
   # Load the json file passed in as a string
-  constructor: (@json) ->
+  constructor: (json_url, options) ->
+    # Allows for alternative syntax
+    if arguments.length is 1 and typeof json_url isnt "string"
+      options = json_url
+      json_url = options.json
     @data = null
-    @isReady = false
-    @setPath()
-    $.getJSON @json, (@data) =>
-      @checkUrl()
+    # Promises
+    @_waiting_for_ready = $.Deferred()
+    @_loading_JSON = $.getJSON(json_url)
+      .done( (@data) => )
+    @_waiting_for_all_done = $.when(@_waiting_for_ready, @_loading_JSON)
+      .done( => @checkUrl() )
+    # Allows an object to preset parameters and immediatly declare ready state.
+    if options?
+      @setPath(options.path)
+      @onError = options.onError
+      @onRedirect = options.onRedirect
+      @output_function = options.output
+      @_waiting_for_ready.resolve()
+    else
+      @setPath()
   # Explicity set the short url path (default: '/s/')
   #
   # Examples:
@@ -36,48 +78,48 @@ class ShortUrl
     if path?
       domain = "#{domain}/" unless domain[domain.length-1] is '/'
       path = path.substring(1) if path[0] is '/'
-      @short_path = "#{domain}#{path}"
+      @path = "#{domain}#{path}"
     else if domain?
-      @short_path = domain
+      @path = domain
     else
-      @short_path = "/s/"
+      @path = "/s/"
+  setOutputWith: (@output_function) ->
+  # Abstact function for testing
+  @redirectTo: (url) -> window.location.href = url
+  # Util function to make output data
+  outputData: (item) ->
+    $.extend item, { short_path: "#{@path}##{item.id}" }
   # Atempt to redirect the browser
   loadLocation: (id) ->
-    if @data[id]?
-      $("#urls-loading").html("""
-        Redirecting to <a href="#{@data[id]}">#{@data[id]}</a>...
-      """)
-      setTimeout ( => window.location.href = @data[id] ), 10
+    item = @data[id]
+    if item?
+      $(@).trigger "redirect", @outputData(item)
+      setTimeout ( => ShortUrl.redirectTo item ), 10
       return true
     else
-      $("#error-id").text(id)
-      $("#error").show()
+      $(@).trigger "error", $.Event("error", { id: id })
       return false
   # Output the list of known short urls when not redirecting
-  output: =>
-    el = $("#urls")
-    for id, url of @data
-      short = "#{@site_path}##{id}"
-      $("<li/>").html("""
-        <b title="#{short}">#{id}</b> - <a href="#{short}">#{url}</a>
-      """).appendTo(el)
-    $("#urls-loading").hide()
+  output: ->
+    if @output_function?
+      @output_function @outputData(item) for item of @data
+    return
+  # Abstact function for testing
+  @getHash: -> window.location.hash
   # Check if requested a redirect or a list
   # If you have a hash value in the URL then it will attempt to redirect to
   # that short url. Otherwise it will output the list on the page.
-  checkUrl: =>
-    return false unless @isReady and @data?
-    id = window.location.hash?.substring(1) or null
-    if id?
+  checkUrl: ->
+    return false unless @_waiting_for_all_done.isResolved()
+    id = ShortUrl.getHash()?.substring(1) or null
+    if id?.length > 1
       @loadLocation id
     else
-      setTimeout @output, 10
+      setTimeout ( => @output() ), 10
     return true
   # Register that checking, redirecting or outputing is ready after all
   # asynchronous tasks are finished.
-  ready: ->
-    @isReady = true
-    @checkUrl()
+  ready: -> @_waiting_for_ready.resolve()
 
 if module?
   # CommonJS
--- source/test/spec/shorturl.spec.js
+++ source/test/spec/shorturl.spec.js
@@ -4,69 +4,239 @@
     var ShortUrl = require("ShortUrl");
 
     beforeEach(function() {
-      spyOn($, "getJSON");
-      this.testObj = new ShortUrl();
+      this.test_loading_JSON = $.Deferred()
+      spyOn($, "getJSON").andReturn(this.test_loading_JSON.promise());
+      spyOn(ShortUrl, "redirectTo");
     });
 
     describe("#constructor", function() {
+      var test_constructor = function() {
+        it("should define @path", function() {
+          expect( this.test.path ).toBeDefined();
+          expect( this.test.path ).toEqual(jasmine.any(String));
+        });
+        it("should deffer when loading JSON", function() {
+          expect( this.test._loading_JSON.state() ).toBe("pending");
+        });
+        it("should deffer when waiting for all done", function() {
+          expect( this.test._waiting_for_all_done.state() ).toBe("pending");
+        });
+      };
+      describe("invocation with path argument", function() {
+        beforeEach(function() {
+          this.test = new ShortUrl("foobar");
+        });
+        test_constructor();
+        it("should deffer when waiting for ready", function() {
+          expect( this.test._waiting_for_ready.state() ).toBe("pending");
+        });
+      });
+      describe("invocation with path and options", function() {
+        beforeEach(function() {
+          this.test = new ShortUrl("foobar", {});
+        });
+        test_constructor();
+        it("should not deffer when waiting for ready", function() {
+          expect( this.test._waiting_for_ready.state() ).toBe("resolved");
+        });
+      });
+      describe("invocation with only options", function() {
+        beforeEach(function() {
+          this.test = new ShortUrl({});
+        });
+        test_constructor();
+        it("should not deffer when waiting for ready", function() {
+          expect( this.test._waiting_for_ready.state() ).toBe("resolved");
+        });
+      });
+    });
+
+    describe("AJAX done", function() {
       beforeEach(function() {
-        spyOn(ShortUrl.prototype, "setPath");
-        this.test = new ShortUrl();
+        this.test = new ShortUrl("foobar");
+        spyOn(this.test, "checkUrl");
       });
-      it("should initialize @ready to false", function() {
-        expect( this.test.isReady ).toBeFalsy();
+      it("should get JSON from path argument", function() {
+        expect( $.getJSON ).toHaveBeenCalledWith("foobar");
       });
-      it("should call setPath", function() {
-        expect( ShortUrl.prototype.setPath ).toHaveBeenCalled();
+      it("should set the data", function() {
+        this.test_loading_JSON.resolve("test_data");
+        expect( this.test.data ).toBe("test_data");
+      });
+      it("should not call checkUrl when not ready", function() {
+        this.test_loading_JSON.resolve("test_data");
+        expect( this.test.checkUrl ).not.toHaveBeenCalled();
+      });
+      it("should call checkUrl when ready", function() {
+        this.test._waiting_for_ready.resolve();
+        this.test_loading_JSON.resolve("test_data");
+        expect( this.test.checkUrl ).toHaveBeenCalled();
       });
     });
 
     describe("#setPath", function() {
+      beforeEach(function() {
+        this.test = new ShortUrl();
+      });
       it("should provide a default", function() {
-        this.testObj.setPath();
-        expect( this.testObj.short_path ).toEqual(jasmine.any(String));
+        this.test.setPath();
+        expect( this.test.path ).toEqual(jasmine.any(String));
       });
       it("should accept one parameter", function() {
-        this.testObj.setPath("foo");
-        expect( this.testObj.short_path ).toBe("foo");
+        this.test.setPath("foo");
+        expect( this.test.path ).toBe("foo");
       });
       it("should accept two parameters", function() {
-        this.testObj.setPath("foo", "bar");
-        expect( this.testObj.short_path ).toContain("foo");
-        expect( this.testObj.short_path ).toContain("bar");
+        this.test.setPath("foo", "bar");
+        expect( this.test.path ).toContain("foo");
+        expect( this.test.path ).toContain("bar");
       });
       it("should concate two parameters with a '/'", function() {
-        this.testObj.setPath("foo", "bar");
-        expect( this.testObj.short_path ).toBe("foo/bar");
-        this.testObj.setPath("foo/", "bar");
-        expect( this.testObj.short_path ).toBe("foo/bar");
-        this.testObj.setPath("foo", "/bar");
-        expect( this.testObj.short_path ).toBe("foo/bar");
-        this.testObj.setPath("foo/", "/bar");
-        expect( this.testObj.short_path ).toBe("foo/bar");
+        this.test.setPath("foo", "bar");
+        expect( this.test.path ).toBe("foo/bar");
+        this.test.setPath("foo/", "bar");
+        expect( this.test.path ).toBe("foo/bar");
+        this.test.setPath("foo", "/bar");
+        expect( this.test.path ).toBe("foo/bar");
+        this.test.setPath("foo/", "/bar");
+        expect( this.test.path ).toBe("foo/bar");
+      });
+    });
+
+    describe("#setOutputWith", function() {
+      beforeEach(function() {
+        this.test = new ShortUrl();
+      });
+      it("should assign output_function", function() {
+        var callback = jasmine.createSpy("output_function");
+        this.test.setOutputWith(callback);
+        expect( this.test.output_function ).toEqual(callback);
+        expect( callback ).not.toHaveBeenCalled();
+      });
+    });
+
+    describe("#redirectTo (static)", function() {
+      it("needs no test");
+    });
+
+    describe("#outputData (static)", function() {
+      beforeEach(function() {
+        this.test = new ShortUrl({
+          json: "test_json",
+          path: "test_path"
+        });
+      });
+      it("should add 'short_path' property to item", function() {
+        var result = this.test.outputData({
+          id: "test_id",
+          url: "test_url"
+        });
+        expect( result.id ).toBeDefined();
+        expect( result.url ).toBeDefined();
+        expect( result.short_path ).toBeDefined();
+        expect( result.short_path ).toContain("test_path");
+        expect( result.short_path ).toContain("#test_id");
       });
     });
 
     describe("#loadLocation", function() {
       beforeEach(function() {
-        spyOn(ShortUrl, "redirectTo");
         jasmine.Clock.useMock();
-      });
-      it("should redirect to proper url", function() {
+        ShortUrl.redirectTo.reset();
+        this.test = new ShortUrl();
+        this.test.data = {"test_id":"test_url"};
       });
       it("should return true when id is found in data", function() {
+        expect( this.test.loadLocation("test_id") ).toBeTruthy();
+      });
+      it("should redirect to proper url", function() {
+        this.test.loadLocation("test_id");
+        expect( ShortUrl.redirectTo ).not.toHaveBeenCalled();
+        jasmine.Clock.tick(10);
+        expect( ShortUrl.redirectTo ).toHaveBeenCalledWith("test_url");
       });
       it("should return false when id is not found in data", function() {
+        expect( this.test.loadLocation("test_bad_id") ).toBeFalsy();
+      });
+      it("should trigger 'redirect' event when id is found in data", function() {
+        var event_callback = jasmine.createSpy("onRedirect");
+        $(this.test).on("redirect", event_callback);
+        this.test.loadLocation("test_id");
+        expect( event_callback ).toHaveBeenCalled();
+      });
+      it("should trigger 'error' when id is not found in data", function() {
+        var event_callback = jasmine.createSpy("onError");
+        $(this.test).on("error", event_callback);
+        this.test.loadLocation("test_bad_id");
+        expect( event_callback ).toHaveBeenCalled();
       });
     });
 
     describe("#output", function() {
+      beforeEach(function() {
+        this.test = new ShortUrl();
+        this.test.data = {"test_id":"test_url"};
+        this.output_callback = jasmine.createSpy("output_callback");
+      });
+      it("should noop when no output callback is defined", function() {
+        var _this = this;
+        expect( function(){ _this.test.output(); } ).not.toThrow();
+      });
+      it("should call the output callback for each item in @data", function() {
+        this.test.setOutputWith(this.output_callback);
+        this.test.output();
+        expect( this.output_callback ).toHaveBeenCalledWith(jasmine.any(Object));
+      });
     });
 
     describe("#checkUrl", function() {
+      beforeEach(function() {
+        jasmine.Clock.useMock();
+        this.test = new ShortUrl();
+        spyOn(ShortUrl, "getHash");
+        spyOn(this.test, "loadLocation");
+        spyOn(this.test, "output");
+        this.resolve = function() {
+          this.test_loading_JSON.resolve("test_data");
+          this.test._waiting_for_ready.resolve();
+        };
+      });
+      it("should return false when not ready", function() {
+        expect( this.test.checkUrl() ).toBeFalsy();
+      });
+      it("should load location when hash has id", function() {
+        this.resolve();
+        ShortUrl.getHash.andReturn("#test_id");
+        this.test.checkUrl();
+        expect( this.test.output ).not.toHaveBeenCalled();
+        expect( this.test.loadLocation ).toHaveBeenCalledWith("test_id");
+      });
+      it("should output when hash is null", function() {
+        this.resolve();
+        ShortUrl.getHash.andReturn("");
+        this.test.checkUrl();
+        expect( this.test.loadLocation ).not.toHaveBeenCalled();
+        expect( this.test.output ).not.toHaveBeenCalled();
+        jasmine.Clock.tick(10);
+        expect( this.test.loadLocation ).not.toHaveBeenCalled();
+        expect( this.test.output ).toHaveBeenCalled();
+      });
     });
 
     describe("#ready", function() {
+      beforeEach(function() {
+        this.test = new ShortUrl("foobar");
+        spyOn(this.test, "checkUrl");
+      });
+      it("should not call checkUrl when AJAX not done", function() {
+        this.test._waiting_for_ready.resolve();
+        expect( this.test.checkUrl ).not.toHaveBeenCalled();
+      });
+      it("should call checkUrl when AJAX done", function() {
+        this.test_loading_JSON.resolve("test_data");
+        this.test._waiting_for_ready.resolve();
+        expect( this.test.checkUrl ).toHaveBeenCalled();
+      });
     });
   });
 
