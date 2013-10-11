674a12aaa76c7083f627ca7048eb507add28e857
Devin Weaver <suki@tritarget.org>
2013-10-11 09:54:24 -0400

    Adds _secrets.yml support to encrypt.rb plugin

--- plugins/encrypt.rb
+++ plugins/encrypt.rb
@@ -6,6 +6,18 @@
 # visiters to see but do not want spiders or robots to see (keep from search
 # engines and spammers). HTML only allowed in block.
 #
+# If you store the Jekyll source publicly (like on Github) You can avoid
+# putting sensitive data inside a `_secrets.yml` file and look them up by id.
+# Add _secrets.yml to your .gitignore to avoid adding it to the repo.
+#
+# DISCLAIMER: This is nothing more then an XOR over each character. Not only
+# that but the same key is used every time. Also, the key is simply the number
+# 4. Lastly the key is part of the output. This have not even the remote chance
+# of being considered secure in any fashion. It's about as good as ROT13. But
+# that is good enough for spammers/bots.
+#
+#   {% encrypt :email %}
+#   OR
 #   {% encrypt 555-1234 %}
 #   OR
 #   {% encryptblock %}
@@ -41,7 +53,7 @@ module Jekyll
       @decrypted_text.each_byte do |i|
         @encrypted_text << (i ^ @xorcode).chr
       end
-      @encrypted_text
+      @encrypted_text = @encrypted_text.gsub(/'/, "\\'")
     end
   end
 
@@ -57,13 +69,30 @@ module Jekyll
   class EncryptTag < Liquid::Tag
 
     def initialize(tag_name, markup, tokens)
-      @markup = markup
       super
+      @markup = markup.lstrip.rstrip
+      @errors = nil
+      if @markup =~ /^:(\w+)/
+        begin
+          secrets = YAML::load_file("_secrets.yml")
+          @markup = secrets[$1]
+        rescue
+          @errors = "Encrypt plugin failed to load _secrets.yml. Unable to lookup #{@markup}."
+        end
+        if @markup.nil?
+          @errors = "Unable to find #{@markup} in _secrets.yml."
+        end
+      end
     end
 
     def render(context)
-      content = Encryptor.new(@markup)
-      content.to_js
+      if @errors.nil?
+        content = Encryptor.new(@markup)
+        content.to_js
+      else
+        puts @errors
+        @errors
+      end
     end
 
   end
