63897e7a88eb08cf20878dabb2a302d35f81dd44
Devin Weaver <suki@tritarget.org>
2014-10-05 03:23:29 -0400

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+public_dir="build" # compiled site directory
+ssh_user="ktohg@tritarget.org"
+ssh_port="22"
+document_root="~/tritarget.org/"
+delete="--delete"
+exclude="--exclude-from ./rsync-exclude"
+
+pushd devin-contact-app
+npm install || exit 1
+npm run build || exit 1
+popd
+
+npm install || exit 1
+npm run build || exit 1
+
+rsync -avze "ssh -p ${ssh_port} ${exclude} ${delete} ${public_dir}/ ${ssh_user}:${document_root}"
