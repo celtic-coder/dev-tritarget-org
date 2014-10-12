0ab183dbd2f0d078ab475148a2e303e8e289ba59
Devin Weaver <suki@tritarget.org>
2014-10-12 18:29:40 -0400

    Add submodule supprt to deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,9 @@ document_root="~/tritarget.org/"
 delete="--delete"
 exclude="--exclude-from ./rsync-exclude"
 
+git submodule init   || exit 1
+git submodule update || exit 1
+
 deploy_build() {
   pushd $1
   npm install          || exit 1
@@ -19,4 +22,10 @@ deploy_build() {
 deploy_build devin-contact-app
 deploy_build ./
 
-echo rsync -avz -e "ssh -p ${ssh_port}" ${exclude} ${delete} ${public_dir}/ ${ssh_user}:${document_root}
+case "$1" in
+  -b|--build)
+    echo "Build done"
+    ;;
+  *)
+    exec rsync -avz -e "ssh -p ${ssh_port}" ${exclude} ${delete} ${public_dir}/ ${ssh_user}:${document_root}
+esac
